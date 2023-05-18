834db29a715f8652600a620370b4dc73599725c2
Olaoluwa Osuntokun <laolu32@gmail.com>
2023-05-17 20:44:00 -0700

    finial: add HTTP Open AI proxy + Aperture config

--- proxy.go
+++ proxy.go
@@ -3,9 +3,12 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/rakyll/openai-go"
@@ -17,6 +20,8 @@ const (
 )
 
 func proxyLLMRequest(w http.ResponseWriter, r *http.Request) {
+	log.Println("ok doing it")
+
 	apiReq, err := UmarshalJsonReq(r.Body)
 	if err != nil {
 		log.Println("unable to parse req: %v", err)
@@ -32,20 +37,65 @@ func proxyLLMRequest(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	s := openai.NewSession(os.Getenv(openAiKey))
 
-	client := chat.NewClient(s, apiReq.Model)
-	apiResp, err := client.CreateCompletion(ctx, apiReq.CreateCompletionParams)
-	if err != nil {
-		log.Fatalf("Failed to complete: %v", err)
-	}
+	completeParams := apiReq.CreateCompletionParams
 
-	log.Println("proxy resp: ", spew.Sdump(apiResp))
+	if !apiReq.Stream {
+		client := chat.NewClient(s, apiReq.Model)
+		apiResp, err := client.CreateCompletion(ctx, completeParams)
+		if err != nil {
+			log.Fatalf("Failed to complete: %v", err)
+		}
 
-	w.Header().Set("Content-Type", "application/json")
+		log.Println("proxy resp: ", spew.Sdump(apiResp))
 
-	err = json.NewEncoder(w).Encode(apiResp)
-	if err != nil {
-		http.Error(w, "Failed to encode response",
-			http.StatusInternalServerError)
-		return
+		w.Header().Set("Content-Type", "application/json")
+
+		err = json.NewEncoder(w).Encode(apiResp)
+		if err != nil {
+			http.Error(w, "Failed to encode response",
+				http.StatusInternalServerError)
+			return
+		}
+
+	} else {
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// Write an SSE comment to establish the SSE stream
+		fmt.Fprint(w, ": SSE stream\n")
+
+		// Flush the response writer to ensure the comment is sent
+		// immediately
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			http.Error(w, "Streaming unsupported",
+				http.StatusInternalServerError)
+			return
+		}
+		flusher.Flush()
+
+		client := chat.NewStreamingClient(s, apiReq.Model)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		client.CreateCompletion(ctx, completeParams, func(r *chat.CreateCompletionStreamingResponse) {
+			// Encode the data as JSON
+			jsonData, err := json.Marshal(r)
+			if err != nil {
+				http.Error(w, "Failed to encode JSON",
+					http.StatusInternalServerError)
+				return
+			}
+
+			// Write the JSON chunk to the SSE stream
+			fmt.Fprintf(w, "data: %s\n\n", jsonData)
+			flusher.Flush()
+
+			// Delay between sending chunks (optional)
+			time.Sleep(1 * time.Second)
+		})
 	}
 }
